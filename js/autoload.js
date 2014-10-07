b3ce5f0c8e785eb66133b91337139bf388c31520
Filip Lindvall <filip9719@gmail.com>
2014-10-07 20:04:22 +0200

    added functionality for autoloading and autoupdating

--- js/autoload.js
+++ js/autoload.js
@@ -1,6 +1,9 @@
 
-function getUrlVars()
+function setVarsFromUrl(force)
 {
+	if(window.vals != undefined && window.vals != null && !force)
+		return;
+
 	var values = [], vals, url = window.location.href;
 
 	var val = url.slice(url.indexOf('?') + 1).split('&');
@@ -9,57 +12,99 @@ function getUrlVars()
 	{
 		vals = val[i].split('=');
 		values.push(vals[0]);
-		values[vals[0]] = vals[1];
+		if(isNaN(parseInt(vals[1])))
+			vals[1] = 0;
+		values[vals[0]] = parseInt(vals[1]);
 	}
-	return values;
+	window.vals = values;
+}
+
+function Vals(name, value)
+{
+	if(value == undefined)
+		return window.vals[name] || "0";
+	else
+		window.vals[name] = parseInt(value);
 }
 
 function startAutoUpdating()
 {
-	window.ajaxUpdateTimer = setInterval(function(){
+	if(window.ajaxUpdateTimer == undefined || window.ajaxUpdateTimer == null)
+		window.ajaxUpdateTimer = setInterval(function(){
 
-		if(topOfPage())
-		{
-
-			var vals = getUrlVars(),
-				userID = 0,
-				length = 1;
-
-			if(vals['filter'] !== undefined)
+			if(topOfPage() && !mutex('autoUpdate'))
 			{
-				userID = vals['filter'];
+
+				var userID = 0,
+					length = 1;
+
+				if(vals['filter'] !== undefined)
+				{
+					userID = Vals("filter");
+				}
+
+				if(vals['offset'] !== undefined)
+					length = Vals("offset") + Vals("length");
+
+				$.ajax({
+					url:"scribbles.php",
+					type:"get",
+					data:{
+						length:length,
+						filter:userID
+					},
+					success:function(result){
+						var toUpdate = $('.scribble-wrapper').find('ul');
+						toUpdate.html(result);
+						Vals("length", length);
+						Vals("offset", "0");
+						setMutex('autoUpdate', 1000);
+					}
+				});
 			}
+		}, 10000);
+}
 
-			if(vals['offset'] !== undefined)
-				length = vals['offset'];
+function stopAutoUpdating()
+{
+	clearInterval(window.ajaxUpdateTimer);
+	window.ajaxUpdateTimer = null;
+}
 
-			$.ajax({
-				url:"scribbles.php",
-				type:"get",
-				data:{
-					length:length,
-					userID:userID
-				},
-				success:function(result){
-			    	console.log(result);
-				}
-			});
+function setMutex(name,time)
+{
+	clearTimeout(window[name]);
+	window[name] = setTimeout(function(){
+		window[name] = null;
+	}, time);
+}
 
-			/*$.ajax({
-				url:'scribbles.php',
-				type:"get",
-				data:{
-					'length':length,
-					'userID':userID
-				}
-			}).done(function(){
-				var toUpdate = $('.scribble-wrapper').find('ul');
-				console.log(resp);
-			});*/
+function mutex(name)
+{
+	if(window[name] === null || window[name] === undefined)
+		return false;
+	else
+		return true;
+}
 
-			alert();
+function loadMore()
+{
+	if(mutex('autoLoad'))
+		return;
+	$.ajax({
+		url:"scribbles.php",
+		type:"get",
+		data:{
+			offset:Vals('length')
+			
+		},
+		success:function(result){
+			var toUpdate = $('.scribble-wrapper').find('ul');
+			toUpdate.append(result);
+			Vals("length", parseInt(Vals("length")) + 1);
+			setMutex('autoLoad', 1000);
 		}
-	}, 2000);
+	});
 }
 
 function topOfPage()
@@ -80,14 +125,12 @@ function bottomOfPage()
 
 
 $(document).ready(function(){
-
+	setVarsFromUrl();
 	startAutoUpdating();
 	$(window).on('scroll', function(){
 		
-		if(topOfPage())
-			console.log("Top of the page");
-		else if(bottomOfPage())
-			console.log("Bottom of the page");
+		if(bottomOfPage())
+			loadMore();
 
 	});
 
